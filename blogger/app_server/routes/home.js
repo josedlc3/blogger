8b00b7d06b4c2699771185ec36f1a206fe3598b6
Jose De La Cruz <jose@track5media.com>
2024-02-27 08:11:54 +0000

    final lab 4-changes WOW (TOOK TOO LONG)

--- blogger/app_server/routes/home.js
+++ blogger/app_server/routes/home.js
@@ -9,12 +9,15 @@ router.get('/', ctrlBlog.homepage);
 router.get('/bloglist', ctrlBlog.bloglist);
 
 /* Blog Add */
-router.get('/blogadd', ctrlBlog.blogadd);
+router.get('/blogadd', ctrlBlog.addBlogs);
+router.post('/blogadd', ctrlBlog.blogadd);
 
 /* Blog Edit */
 router.get('/blogedit/:blogid', ctrlBlog.blogedit);
+router.post('/blogedit/:blogid', ctrlBlog.editBlog);
 
 /* Blog Deletion */
-router.get('/blogdeletion/blogid', ctrlBlog.blogdeletion);
+router.get('/blogdeletion/:blogid', ctrlBlog.deleteBlog);
+router.post('/blogdeletion/:blogid', ctrlBlog.blogdeletion);
 
 module.exports = router;
